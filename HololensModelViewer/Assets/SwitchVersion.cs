b9bc2ef631b53cde41a3fe1e88aa02d241bedf72
ny-kristof <kristof.nyomarkay@gmail.com>
2020-11-19 17:46:20 +0100

    Cleanup

---
+++ HololensModelViewer/Assets/SwitchVersion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchVersion : MonoBehaviour
+{
+    public void Version()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(!child.gameObject.activeSelf);
+        }
+    }
+}
